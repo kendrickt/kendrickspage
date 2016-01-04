a86e2e847999d1bc8376d36345468d74f927e2d2
kendricktang <kentangsrice@gmail.com>
2016-01-03 21:25:32 -0800

    adds '/test' page which tests submitting forms without refreshing the page, and using ajax to update a part of the html.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 from app import app
 from .forms import TestForm
-from testapp import add
+from testapp import add as add2ints
 
 
 @app.route('/')
@@ -24,9 +24,21 @@ def specific_projects(projectname):
     return render_template('projects/%s.html' % projectname)
 
 
-@app.route("/test", methods=['GET', 'POST'])
+@app.route('/test', methods=['GET', 'POST'])
 def test():
     form = TestForm()
     if form.validate_on_submit():
-        return str(add.add_2_ints(form.int_a.data, form.int_b.data))
+        return str(add2ints.add_2_ints(form.int_a.data, form.int_b.data))
     return render_template('test.html', form=form)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    return jsonify({
+        'sum': str(
+            add2ints.add_2_ints(
+                int(request.form['a']),
+                int(request.form['b'])
+            )
+        )
+    })
---
+++ app/static/js/test.js
@@ -0,0 +1,10 @@
+function add() {
+    $.post('/add', {
+        a: +document.getElementById('int_a').value,
+        b: +document.getElementById('int_b').value
+    }).done(function(added) {
+        document.getElementById('the-sum').innerHTML = added['sum'];
+    }).fail(function() {
+        alert('oh no.');
+    });
+}
