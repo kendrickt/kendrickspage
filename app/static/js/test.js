fc148b5d3fbfb610336cdccc48e7afd42e17469a
kendricktang <kentangsrice@gmail.com>
2016-01-14 19:33:21 -0800

    adds 4th post about receivers. Adds boarders to images.

--- app/static/js/test.js
+++ app/static/js/test.js
@@ -40,3 +40,30 @@ function makeplot_20160103() {
         alert('oh no.');
     });
 }
+
+function scroll_images(id, dir) {
+    var images = $(id).children();
+    var curr;
+    var image;
+    for (i = 0; i < images.length; i++) {
+        image = images[i];
+        if (image.className != 'no-display') {
+            curr = i;
+            break;
+        }
+    }
+    if (dir == -1 && curr == 0) {
+        alert('I can\'t let you do that, Starfox. No plots for before 2012.');
+        return;
+    } else if (dir == 1 && curr == 3) {
+        alert('Can\'t go into the future, friend.');
+        return;
+    } else if (dir == 1) {
+        image = images[curr+1];
+    } else {
+        image = images[curr-1];
+    }
+    image.className = '';
+    image = images[curr];
+    image.className = 'no-display';
+}
